2c32214c87cbae72c929a51b9f25682dfb3783b5
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-09-18 13:50:04 -0300

    requisito 3 completo

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,13 +1,111 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PlanetsContext from '../context/DataContext';
 
 function Table() {
   const { planetData, searchTerm, setSearchTerm } = useContext(PlanetsContext);
-  const filteredPlanets = planetData
+  const [filterList, setFilterList] = useState([]);
+  const [selectedColumn, setSelectedColumn] = useState('population');
+  const [selectedComparison, setSelectedComparison] = useState('maior que');
+  const [filterValue, setFilterValue] = useState('0');
+
+  const columnOrder = [
+    'name',
+    'climate',
+    'diameter',
+    'gravity',
+    'orbital_period',
+    'population',
+    'rotation_period',
+    'surface_water',
+    'terrain',
+    'films',
+    'created',
+    'edited',
+    'url',
+  ];
+  const handleColumnChange = (event) => {
+    setSelectedColumn(event.target.value);
+  };
+
+  const handleComparisonChange = (event) => {
+    setSelectedComparison(event.target.value);
+  };
+
+  const handleValueChange = (event) => {
+    setFilterValue(event.target.value);
+  };
+
+  const handleAddFilter = () => {
+    if (selectedColumn !== '' && selectedComparison !== '' && filterValue !== 0) {
+      setFilterList([
+        ...filterList,
+        {
+          column: selectedColumn,
+          comparison: selectedComparison,
+          value: filterValue,
+        },
+      ]);
+    }
+  };
+
+  const filteredPlanetsByName = planetData
     .filter((planet) => planet.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  const filteredPlanets = filteredPlanetsByName.filter((planet) => {
+    return filterList.every((filter) => {
+      const value = parseFloat(planet[filter.column]);
+      const filterValueList = parseFloat(filter.value);
+
+      if (filter.comparison === 'maior que') {
+        return value > filterValueList;
+      } if (filter.comparison === 'menor que') {
+        return value < filterValueList;
+      } if (filter.comparison === 'igual a') {
+        return value === filterValueList;
+      }
+
+      return true;
+    });
+  });
+
   return (
     <>
       <div>
+        <select
+          data-testid="column-filter"
+          value={ selectedColumn }
+          onChange={ handleColumnChange }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+
+        <select
+          data-testid="comparison-filter"
+          value={ selectedComparison }
+          onChange={ handleComparisonChange }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+
+        <input
+          type="number"
+          value={ filterValue }
+          onChange={ handleValueChange }
+          data-testid="value-filter"
+        />
+
+        <button
+          onClick={ handleAddFilter }
+          data-testid="button-filter"
+        >
+          Adicionar Filtro
+        </button>
         <input
           type="text"
           value={ searchTerm }
@@ -37,14 +135,13 @@ function Table() {
         <tbody>
           {filteredPlanets.map((planet, index) => (
             <tr key={ index }>
-              {Object.values(planet).map((value, mapIndex) => (
-                <td key={ mapIndex }>{value}</td>
+              {columnOrder.map((columnName) => (
+                <td key={ columnName }>{planet[columnName]}</td>
               ))}
             </tr>
           ))}
         </tbody>
       </table>
-
     </>
   );
 }
